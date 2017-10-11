5a647482c366b8acf3c8116bd2db0bff25a18738
hjd919 <297538600@qq.com>
2017-10-11 15:14:50 +0800

    lala

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
-
     protected $guarded = [];
 
     public function isNew($openid)
